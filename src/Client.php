c3d6a99c82581800cf315682f950067341ac934d
Claudio Sanches <contato@claudiosmweb.com>
2016-01-04 19:31:10 -0200

    Fixed some coding standards and start add inline docs

--- src/Client.php
+++ src/Client.php
@@ -1,29 +1,99 @@
 <?php
+/**
+ * WooCommerce REST API Client
+ *
+ * @category Client
+ * @package  Automattic/WooCommerce
+ * @author   Claudio Sanches <claudio.sanches@automattic.com>
+ * @license  https://opensource.org/licenses/MIT MIT
+ * @link     https://github.com/woothemes/wc-api-php
+ */
+
 namespace Automattic\WooCommerce;
 
 use Automattic\WooCommerce\Request\Request;
 
-class Client {
+/**
+ * REST API Client class.
+ *
+ * @category Client
+ * @package  Automattic/WooCommerce
+ * @author   Claudio Sanches <claudio.sanches@automattic.com>
+ * @license  https://opensource.org/licenses/MIT MIT
+ * @link     https://github.com/woothemes/wc-api-php
+ */
+class Client
+{
 
-    private $request;
+    /**
+     * Instance of Automattic\WooCommerce\Request\Request.
+     *
+     * @var Request
+     */
+    private $_request;
 
-    public function __construct($url, $consumerKey, $consumerSecret, $options) {
-        $this->request = new Request($url, $consumerKey, $consumerSecret, $options);
+    /**
+     * Initialize client.
+     *
+     * @param string $url            Store URL.
+     * @param string $consumerKey    Consumer key.
+     * @param string $consumerSecret Consumer secret.
+     * @param array  $options        Options (version, timeout, verify_ssl).
+     */
+    public function __construct($url, $consumerKey, $consumerSecret, $options)
+    {
+        $this->_request = new Request($url, $consumerKey, $consumerSecret, $options);
     }
 
-    public function post($endpoint, $data) {
+    /**
+     * POST method.
+     *
+     * @param string $endpoint API endpoint.
+     * @param array  $data     Request data.
+     *
+     * @return array
+     */
+    public function post($endpoint, $data)
+    {
 
     }
 
-    public function put($endpoint, $data) {
+    /**
+     * PUT method.
+     *
+     * @param string $endpoint API endpoint.
+     * @param array  $data     Request data.
+     *
+     * @return array
+     */
+    public function put($endpoint, $data)
+    {
 
     }
 
-    public function get($endpoint, $params) {
+    /**
+     * GET method.
+     *
+     * @param string $endpoint API endpoint.
+     * @param array  $params   Request params.
+     *
+     * @return array
+     */
+    public function get($endpoint, $params)
+    {
 
     }
 
-    public function delete($endpoint, $params) {
+    /**
+     * DELETE method.
+     *
+     * @param string $endpoint API endpoint.
+     * @param array  $params   Request params.
+     *
+     * @return array
+     */
+    public function delete($endpoint, $params)
+    {
 
     }
 }
--- src/Request/Request.php
+++ src/Request/Request.php
@@ -1,7 +1,27 @@
 <?php
+/**
+ * WooCommerce REST API Request
+ *
+ * @category Request
+ * @package  Automattic/WooCommerce
+ * @author   Claudio Sanches <claudio.sanches@automattic.com>
+ * @license  https://opensource.org/licenses/MIT MIT
+ * @link     https://github.com/woothemes/wc-api-php
+ */
+
 namespace Automattic\WooCommerce\Request;
 
-class Request {
+/**
+ * REST API Request class.
+ *
+ * @category Request
+ * @package  Automattic/WooCommerce
+ * @author   Claudio Sanches <claudio.sanches@automattic.com>
+ * @license  https://opensource.org/licenses/MIT MIT
+ * @link     https://github.com/woothemes/wc-api-php
+ */
+class Request
+{
 
     const VERSION = 'v3';
     protected $url;
@@ -12,7 +32,8 @@ class Request {
     protected $verifySsl;
     protected $timeout;
 
-    public function __construct($url, $consumerKey, $consumerSecret, $options) {
+    public function __construct($url, $consumerKey, $consumerSecret, $options)
+    {
         $this->version        = $this->getVersion($options);
         $this->isSsl          = $this->isSsl($url);
         $this->url            = $this->buildApiUrl($url);
@@ -22,27 +43,33 @@ class Request {
         $this->timeout        = $this->getTimeout($options);
     }
 
-    protected function getVersion($options) {
-        return isset( $options['version'] ) ? $options['version'] : self::VERSION;
+    protected function getVersion($options)
+    {
+        return isset($options['version']) ? $options['version'] : self::VERSION;
     }
 
-    protected function isSsl($url) {
+    protected function isSsl($url)
+    {
         return 'https://' === \substr($url, 0, 8);
     }
 
-    protected function buildApiUrl($url) {
-        return \rtrim( $url, '/' ) . '/wc-api/' + $this->version + '/';
+    protected function buildApiUrl($url)
+    {
+        return \rtrim($url, '/') . '/wc-api/' + $this->version + '/';
     }
 
-    protected function verifySsl($options) {
-        return isset( $options['verify_ssl'] ) ? (bool) $options['verify_ssl'] : true;
+    protected function verifySsl($options)
+    {
+        return isset($options['verify_ssl']) ? (bool) $options['verify_ssl'] : true;
     }
 
-    protected function getTimeout($options) {
-        return isset( $options['timeout'] ) ? (int) $options['timeout'] : 15;
+    protected function getTimeout($options)
+    {
+        return isset($options['timeout']) ? (int) $options['timeout'] : 15;
     }
 
-    public function request($endpoint, $method, $data = [], $params = []) {
+    public function request($endpoint, $method, $data = [], $params = [])
+    {
         $ch       = \curl_init();
         $request  = [];
         $response = [];
@@ -53,16 +80,16 @@ class Request {
             'User-Agent: WooCommerce API Client-PHP/1.0',
         ];
 
-        \curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, $this->verifySsl );
-        \curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, $this->verifySsl );
-        \curl_setopt( $ch, CURLOPT_CONNECTTIMEOUT, $this->timeout );
-        \curl_setopt( $ch, CURLOPT_TIMEOUT, $this->timeout );
-        \curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-        \curl_setopt( $ch, CURLOPT_HTTPHEADER, $request['headers'] );
+        \curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, $this->verifySsl);
+        \curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, $this->verifySsl);
+        \curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $this->timeout);
+        \curl_setopt($ch, CURLOPT_TIMEOUT, $this->timeout);
+        \curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        \curl_setopt($ch, CURLOPT_HTTPHEADER, $request['headers']);
 
-        $response['body'] = \curl_exec( $ch );
+        $response['body'] = \curl_exec($ch);
 
-        \curl_close( $ch );
+        \curl_close($ch);
 
         return [
             'request'  => $request,
